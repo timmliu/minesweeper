1c136672c1733704aa3a492a64012c9cf84eb134
Timm <timmcliu@gmail.com>
2018-10-18 16:07:31 -0700

    simpler bomb collision prevent code

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -27,14 +27,12 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 
   let numberOfBombsPlaced = 0
   while (numberOfBombsPlaced < numberOfBombs) {
-    let randomRowIndex, randomColumnIndex
-    do {
-      randomRowIndex = randomIndex(numberOfRows)
-      randomColumnIndex = randomIndex(numberOfColumns)
-    } while (board[randomRowIndex][randomColumnIndex] === 'B')
-    board[randomRowIndex][randomColumnIndex] = 'B'
-    numberOfBombsPlaced++
-    // !!! The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
+    let randomRowIndex = randomIndex(numberOfRows)
+    let randomColumnIndex = randomIndex(numberOfColumns)
+    if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B'
+      numberOfBombsPlaced++
+    }
   }
 
   return board
