1fd7dcc61bcf9f997f609ec529aef3c65d96dfb8
Timm <timmcliu@gmail.com>
2018-10-18 15:36:59 -0700

    dynamically generate game boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -7,7 +7,44 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     }
     board.push(row)
   }
-  return console.log(board)
+  console.log(board)
+  return board
 }
 
-// generatePlayerBoard(2,3)
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = []
+  for (let i = 0; i < numberOfRows; i++) {
+    const row = []
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null)
+    }
+    board.push(row)
+  }
+
+  let numberOfBombsPlaced = 0
+  while (numberOfBombsPlaced < numberOfBombs) {
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows)
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns)
+    board[randomRowIndex][randomColumnIndex] = 'B'
+    numberOfBombsPlaced++
+    // !!! The code in your while loop has the potential to place bombs on top of already existing bombs. This will be fixed when you learn about control flow.
+  }
+
+  return board
+}
+
+const printBoard = (board) => {
+  console.log(
+    board.map(row => {
+      return row.join(' | ')
+    }).join('\n')
+  )
+}
+
+const playerBoard = generatePlayerBoard(3,4)
+const bombBoard = generateBombBoard(3,4,5)
+
+console.log('Player Board: ')
+printBoard(playerBoard)
+console.log('Bomb Board: ')
+printBoard(bombBoard)
