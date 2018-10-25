b70dd8f33209f3c7c5626984f8089cfcc6369a2f
Timm <timmcliu@gmail.com>
2018-10-25 15:09:02 -0700

    Game.playMove

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -2,6 +2,10 @@ class Game {
   constructor(numberOfRows, numberOfColumns, numberOfBombs) {
     this._board = new Board(numberOfRows, numberOfColumns, numberOfBombs)
   }
+
+  playMove(rowIndex, columnIndex) {
+    this._board.flipTile(rowIndex, columnIndex)
+  }
 }
 
 class Board {
